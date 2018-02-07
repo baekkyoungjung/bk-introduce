c3efc4f2bcc135abe5807ef3e8347fa9f75f9c03
BK <baekkyoungjung>
2018-02-07 16:53:19 +0900

    하.. 쓸까말까

--- src/js/bkbjwedding.js
+++ src/js/bkbjwedding.js
@@ -1,19 +1,5 @@
-const d = document;
-const w = window;
-
-w.onload = () => {
-	// imgOnload();
-	w.addEventListener('scroll', () => {
-		if (w.pageYOffset + w.innerHeight >= d.body.scrollHeight) {
-			d.getElementById('memories').style.top = `${w.innerHeight - 50}px`;
-		} else {
-			d.getElementById('memories').style.top = '100%';
-		}
-	});
-	setTimeout(() => {
-		d.getElementById('invite-says').classList.add('move-down');
-	}, 1500);
-};
+// const d = document;
+// const w = window;
 
 // const promiseImgSrc = src => {
 // 	return new Promise((resolve, reject) => {
@@ -45,39 +31,50 @@ w.onload = () => {
 // 	});
 // };
 
-d.getElementById('memories-toggle-btn').addEventListener('click', () => {
-	d.getElementById('wrapper').classList.toggle('memories-opened');
-	if (d.getElementById('wrapper').className.indexOf('memories-opened') === -1) {
-		w.scroll(0, d.body.scrollHeight);
-	}
-});
+// w.onload = () => {
+// 	// imgOnload();
+// 	w.addEventListener('scroll', () => {
+// 		if (w.pageYOffset + w.innerHeight >= d.body.scrollHeight) {
+// 			d.getElementById('memories').style.top = `${w.innerHeight - 50}px`;
+// 		} else {
+// 			d.getElementById('memories').style.top = '100%';
+// 		}
+// 	});
+// };
 
-w.initMap = () => {
-	const nuHouse = { lat: 37.515812, lng: 127.033657 };
-	const map = new google.maps.Map(d.getElementById('map'), {
-		zoom: 16,
-		center: nuHouse,
-		draggable: false,
-		scrollwheel: false,
-		disableDoubleClickZoom: true,
-		zoomControl: false,
-		mapTypeControl: false,
-		scaleControl: false,
-		rotateControl: false,
-		fullscreenControl: false,
-	});
-	const marker = new google.maps.Marker({
-		position: nuHouse,
-		map,
-		draggable: false,
-		animation: google.maps.Animation.DROP,
-	});
-	const toggleBounce = () => {
-		if (marker.getAnimation() !== null) {
-			marker.setAnimation(null);
-		} else {
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-		}
-	};
-    marker.addListener('click', toggleBounce);
-};
+// d.getElementById('memories-toggle-btn').addEventListener('click', () => {
+// 	d.getElementById('wrapper').classList.toggle('memories-opened');
+// 	if (d.getElementById('wrapper').className.indexOf('memories-opened') === -1) {
+// 		w.scroll(0, d.body.scrollHeight);
+// 	}
+// });
+
+// w.initMap = () => {
+// 	const nuHouse = { lat: 37.515812, lng: 127.033657 };
+// 	const map = new google.maps.Map(d.getElementById('map'), {
+// 		zoom: 16,
+// 		center: nuHouse,
+// 		draggable: false,
+// 		scrollwheel: false,
+// 		disableDoubleClickZoom: true,
+// 		zoomControl: false,
+// 		mapTypeControl: false,
+// 		scaleControl: false,
+// 		rotateControl: false,
+// 		fullscreenControl: false,
+// 	});
+// 	const marker = new google.maps.Marker({
+// 		position: nuHouse,
+// 		map,
+// 		draggable: false,
+// 		animation: google.maps.Animation.DROP,
+// 	});
+// 	const toggleBounce = () => {
+// 		if (marker.getAnimation() !== null) {
+// 			marker.setAnimation(null);
+// 		} else {
+// 			marker.setAnimation(google.maps.Animation.BOUNCE);
+// 		}
+// 	};
+//     marker.addListener('click', toggleBounce);
+// };
